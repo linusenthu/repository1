f602b659d4563fcd245d829867ed574a9d70db5b
linusenthu <36707393+linusenthu@users.noreply.github.com>
2019-09-28 19:09:51 +0530

    Update config.tf

--- config.tf
+++ config.tf
@@ -1,18 +1,9 @@
-provider "docker" {
-  host = "tcp://docker:2345/"
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
 }
-resource "docker_image" "nginx" {
-  name = "nginx:1.11-alpine"
-}
-resource "docker_container" "nginx-server" {
-  name = "nginx-server"
-  image = "${docker_image.nginx.latest}"
-  ports {
-    internal = 80
-  }
-  volumes {
-    container_path  = "/usr/share/nginx/html"
-    host_path = "/home/scrapbook/tutorial/www"
-    read_only = true
-  }
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
 }
